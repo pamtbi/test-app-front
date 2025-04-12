285bd771f484e5d78cfc4efc00fb4b47857669e6
Pavlo Matviienko <pavelmatvienkomatvi@gmail.com>
2025-04-13 01:32:32 +0300

    push

--- src/utils/link.ts
+++ src/utils/link.ts
@@ -1,6 +1,6 @@
 // const base = "http://localhost:5000"
 
-const base = "https://test-app-back.vercel.app";
+const base = "https://test-app-back-idnt.vercel.app";
 
 export const link = (str: string) => {
   return `${base}${str}`
